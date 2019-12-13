5cf600c11e4d44d7299c4121509ad67239f8c5c4
oatezz <oatthanisorn13@gmail.com>
2019-12-13 23:11:34 +0700

    edit upload

--- stm32-light-blink/src/main.ino
+++ stm32-light-blink/src/main.ino
@@ -1,24 +1,49 @@
-#define RED_PIN PB1
-#define GREEN_PIN PB0
-#define BLUE_PIN PA7
 
-int RED = 255, GREEN = 255, BLUE = 255;
+#include <Arduino.h>
+//#ifndef
+#define COMMON_ANODE
+//#define Max 255
+
+#define RED 19
+#define GREEN 18
+#define BLUE 17
+//#endif
+
+int Max = 255;
+//int RED, GREEN, BLUE;
+int setColor(RED, GREEN, BLUE);
 
 void setup(){
-	Serial.begin(115200);
-	pinMode(RED_PIN, OUTPUT);
-	pinMode(GREEN_PIN, OUTPUT);
-	pinMode(BLUE_PIN, OUTPUT);
+	//Serial.begin(115200);
+	pinMode(RED, OUTPUT);
+	pinMode(GREEN, OUTPUT);
+	pinMode(BLUE, OUTPUT);
 
-	setLed(RED,GREEN,BLUE);
+	//pinMode(LED_BUILIN, OUTPUT);
+	//analogWrite(LED_BUILIN, LOW);
+	//setLed(RED,GREEN,BLUE);
+	//setColor(RED,GREEN,BLUE);
 }
 
 void loop(){
-	
-	REB_FADE(100);
+	// RGB fade red to green
+	for(int i=0; i<Max; i++){
+		setColor(Max-i, i, 0);
+	}
+
+	// RGB fade green to blue
+	for(int i=0; i<Max; i++){
+		setColor(0, Max-i, i);
+	}
+
+	// RGB fade blue to red
+	for(int i=0; i<Max; i++){
+		setColor(i, 0, Max-i);
+	}
+	//RGB_FADE(100);
 }
 
-void RGB_FADE(){ 
+/*void RGB_FADE(){ 
 	// RGB fade red to green
 	for(int i=0; i<Max; i++){
 		setColor(Max-i, i, 0);
@@ -34,16 +59,19 @@ void RGB_FADE(){
 		setColor(i, 0, Max-i);
 	}
 
-}
+}*/
 
-void setLed(int RED, int GREEN, int BLUE){
+void setLed(int R, int G, int B){
 	#ifdef COMMON_ANODE
-		RED = Max - RED;
-		GREEN = Max - GREEN;
-		BLUE = Max - BLUE;
+		R = Max - R;
+		G = Max - G;
+		B = Max - B;
 	#endif
 
-	analogWrite(RED_PIN, RED);
-	analogWrite(GREEN_PIN, GREEN);
-	analogWrite(BLUE_PIN, BLUE);
+	analogWrite(RED, R);
+	delay(10);
+	analogWrite(GREEN, G);
+	delay(10);
+	analogWrite(BLUE, B);
+	delay(10);
 }
